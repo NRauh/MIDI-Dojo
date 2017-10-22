aefdb9d099b29d813eafeceff8165c9b26517492
Nate Rauh <nrauh@users.noreply.github.com>
2017-10-22 18:33:44 -0500

    Playing a note when a key is pressed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import * as Tone from 'tone';
+
 const keys = document.querySelectorAll('.keyboard .keys .key');
 
+let synth = new Tone.Synth().toMaster();
+
 keys.forEach((el) => {
-  el.addEventListener('mousedown', () => {
-    console.log('key pressed');
+  el.addEventListener('mousedown', (e) => {
+    synth.triggerAttackRelease(el.dataset.note, '8n');
   });
 });
